1918d1e024bdeadbb313fd0e269878bbbe786029
Bell Eapen <github_public@gulfdoctor.net>
2023-12-12 19:55:30 +0000

    Refactor test functions and add patient_id fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,9 +8,29 @@
 """
 
 import pytest
+import os
+import requests
+import json
 
 @pytest.fixture
 def f():
     from src.medprompt import MedPrompter
     _m = MedPrompter()
     return _m
+
+@pytest.fixture
+def patient_id():
+    url = os.environ.get("FHIR_SERVER_URL", 'http://hapi.fhir.org/baseR4')
+    _patient = requests.get(url + "/Patient?_pretty=true")
+    patient = json.loads(_patient.text)
+    patient_id = patient["entry"][0]["resource"]["id"]
+    for root, dirs, files in os.walk("tests/data"):
+        for file in files:
+            if file.endswith(".json"):
+                fhir_json = open(os.path.join(root, file)).read()
+                resource = json.loads(fhir_json)
+                resource["subject"]["reference"] = "Patient/"+patient_id
+                headers = {'Content-Type': 'application/json'}
+                _post_url = url + "/" + resource["resourceType"]
+                response = requests.post(_post_url, data=json.dumps(resource), headers=headers)
+    return patient_id
--- tests/tools/test_create_embedding.py
+++ tests/tools/test_create_embedding.py
@@ -2,51 +2,31 @@ import pytest
 from src.medprompt.tools.create_embedding import CreateEmbeddingFromFhirBundle
 import os
 import requests
+import json
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
 
 
-def test_post_data_to_fhir():
-    url = os.environ.get("FHIR_SERVER_URL", 'http://hapi.fhir.org/baseR4')
-    fhir_json = open("tests/data/fhir_bundle.json").read()
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=fhir_json, headers=headers)
-    assert response.status_code in [200, 201]
-
-
-def test_create_embedding_from_fhir_bundle():
+def test_create_embedding_from_fhir_bundle(patient_id):
     # Initialize the class
     create_embedding = CreateEmbeddingFromFhirBundle()
 
     # Test the _run method
-    result = create_embedding._run(patient_id="41399401")
+    result = create_embedding._run(patient_id=patient_id)
     print(result)
     assert result is not None
 
     # Test the _arun method
-    # result = create_embedding._arun(patient_id="41399401")
-    # assert result is not None
+    result = create_embedding._arun(patient_id=patient_id)
+    assert result is not None
 
-def test_created_embedding():
+def test_created_embedding(patient_id):
     EMBED_MODEL = os.getenv("EMBED_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
     embedding = HuggingFaceEmbeddings(model_name=EMBED_MODEL)
-    vectorstore = Chroma(collection_name="41399401", persist_directory=os.getenv("CHROMA_DIR", "/tmp/chroma"), embedding_function=embedding)
+    vectorstore = Chroma(collection_name=patient_id, persist_directory=os.getenv("CHROMA_DIR", "/tmp/chroma"), embedding_function=embedding)
     retriever = vectorstore.as_retriever()
     result = vectorstore.similarity_search("Erythrocytes", k=10)
-    # print(result)
     assert result is not None
     retreived = retriever.get_relevant_documents("Erythrocytes", k=10)
     print(retreived)
-
-
-# def test_create_embedding_from_fhir_bundle_with_invalid_patient_id():
-#     # Initialize the class
-#     create_embedding = CreateEmbeddingFromFhirBundle()
-
-#     # Test the _run method with invalid patient_id
-#     with pytest.raises(Exception):
-#         create_embedding._run(patient_id="invalid_patient_id")
-
-#     # Test the _arun method with invalid patient_id
-#     with pytest.raises(Exception):
-#         create_embedding._arun(patient_id="invalid_patient_id")
+    assert retreived is not None
