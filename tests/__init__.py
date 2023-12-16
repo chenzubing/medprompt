2d94f0abff17a2bcbedbf841a5b468e49f440db8
Bell Eapen <github@gulfdoctor.net>
2023-12-16 07:09:34 -0500

    Add design principles and update dependencies

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,7 @@
 from kink import di
 from os import getenv
-from src.medprompt.utils.hapi_server import HapiFhirServer
-from src.medprompt.tools.get_medical_record import GetMedicalRecordTool
+from src.medprompt.utils import HapiFhirServer
+from src.medprompt.tools import GetMedicalRecordTool
 
 di["fhir_server"] = HapiFhirServer()
 di["patient_id"] = getenv("PATIENT_ID", "592911")
