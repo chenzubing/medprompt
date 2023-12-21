1ce3c08cbdac24e5e0361f0a3173a6688230d7a4
Bell Eapen <github_public@gulfdoctor.net>
2023-12-21 17:10:19 +0000

    Add import statement for json module

--- src/medprompt/chains/clinical_summary.py
+++ src/medprompt/chains/clinical_summary.py
@@ -17,6 +17,7 @@
 
 from typing import List
 from kink import di
+import json
 from langchain.prompts.prompt import PromptTemplate
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import RunnableMap, RunnablePassthrough
@@ -29,15 +30,29 @@ from .. import MedPrompter
 med_prompter = MedPrompter()
 import logging
 _logger = logging.getLogger(__name__)
-class ClinicalConceptOutput(BaseModel):
-    clinical_concepts: List[str] = Field()
 
-class ClinicalConceptInput(BaseModel):
-    clinical_document: str = Field()
-    word_count: str = Field()
+main_llm = di["rag_chain_main_llm"]
+clinical_llm = di["rag_chain_clinical_llm"]
 
-CLINICAL_CONCEPT_INPUT_TEMPLATE = """
-Given the following clinical document, extract the clinical concepts and return them as a list.
+rail_spec = """
+<rail version="0.1">
+<output>
+    <object name="response" format="length: 1">
+        <string
+            name="concepts"
+            description="The list of concepts extracted from the clinical document."
+            format="list"
+            on-fail-length="reask"
+        />
+    </object>
+</output>
+
+
+<prompt>
+
+Given the following clinical document, extract all the clinical concepts and return them as a list.
+If concepts are repeated, return them only once.
+If concepts have two words, return them as a single word joined by an underscore.
 The clinical concepts include the following:
 - Problem
 - Medication
@@ -48,45 +63,63 @@ The clinical concepts include the following:
 
 Clinical Document: ${clinical_document}
 ${gr.complete_json_suffix_v2}
+
+</prompt>
+</rail>
+
 """
 
-clinical_concepts_output_parser = GuardrailsOutputParser.from_pydantic(output_class=ClinicalConceptOutput, prompt=CLINICAL_CONCEPT_INPUT_TEMPLATE)
+
+class ClinicalConceptInput(BaseModel):
+    clinical_document: str = Field()
+    word_count: str = Field()
+
+
+
+clinical_concepts_output_parser = GuardrailsOutputParser.from_rail_string(rail_spec, api=main_llm)
 
 CLINICAL_CONCEPT_INPUT_PROMPT = PromptTemplate(
     template=clinical_concepts_output_parser.guard.prompt.escape(),
     input_variables=["clinical_document"],
 )
 
-CLINICAL_CONCEPT_SUMMARY_TEMPLATE = """
-Given the following clinical concepts, summarize them into a single paragraph of ${input.word_count} words.
-Include comments on these ${input.clinical_concepts}.
+# CLINICAL_CONCEPT_SUMMARY_TEMPLATE = """
+# Given the following clinical concepts, summarize them into a single paragraph of ${input.word_count} words.
+# Include comments on these ${input.clinical_concepts}.
 
-Clinical Document: ${input.clinical_document}
-"""
+# Clinical Document: ${input.clinical_document}
+# """
 
-CLINICAL_CONCEPT_SUMMARY_PROMPT = PromptTemplate(
-    template=CLINICAL_CONCEPT_SUMMARY_TEMPLATE,
-    input_variables=["input"],
-)
+# CLINICAL_CONCEPT_SUMMARY_PROMPT = PromptTemplate(
+#     template=CLINICAL_CONCEPT_SUMMARY_TEMPLATE,
+#     input_variables=["input"],
+# )
 
-main_llm = di["rag_chain_main_llm"]
-clinical_llm = di["rag_chain_clinical_llm"]
+
+def extract_concepts(guardrails_output):
+    """Extract the concepts from the clinical document."""
+    _gr = json.loads(guardrails_output)
+    return _gr["response"]
+    # concepts = _gr["response"]["concepts"]
+    # return concepts
+
 
 def get_runnable(**kwargs):
     """Get the runnable chain."""
     list_of_concepts = RunnablePassthrough.assign(
         clinical_document=lambda x: x["clinical_document"],
-    ) | CLINICAL_CONCEPT_INPUT_PROMPT | clinical_llm | clinical_concepts_output_parser | ExpandConceptsTool().with_types(input_type=ClinicalConceptOutput)
-    input = RunnablePassthrough.assign(
-        clinical_document=lambda x: x["clinical_document"],
-        word_count=lambda x: x["word_count"],
-    )
-    _inputs = RunnableMap(
-        clinical_concepts=list_of_concepts,
-        input=input,
-    )
-    _chain = _inputs | CLINICAL_CONCEPT_SUMMARY_PROMPT | main_llm | StrOutputParser()
-    chain = _chain.with_types(input_type=ClinicalConceptInput)
+    ) | CLINICAL_CONCEPT_INPUT_PROMPT | main_llm | StrOutputParser() | extract_concepts |  ExpandConceptsTool().run
+    # input = RunnablePassthrough.assign(
+    #     clinical_document=lambda x: x["clinical_document"],
+    #     word_count=lambda x: x["word_count"],
+    # )
+    # _inputs = RunnableMap(
+    #     clinical_concepts=list_of_concepts,
+    #     input=input,
+    # )
+    # _chain = _inputs | CLINICAL_CONCEPT_SUMMARY_PROMPT | main_llm | StrOutputParser()
+    # chain = _chain.with_types(input_type=ClinicalConceptInput)
+    chain = list_of_concepts
     return chain
 
 @tool("clinical summary", args_schema=ClinicalConceptInput)
